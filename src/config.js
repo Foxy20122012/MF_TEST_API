959e0ac856c0a09b69d9e23f1ce324b06f953045
Miguel Enrique Alvarez Merlos <miguel.alvarez@fiesta.local>
2024-06-19 00:38:20 -0600

    agregando variables de entorno

--- src/config.js
+++ src/config.js
@@ -1,9 +1,9 @@
 import { config } from "dotenv";
 config();
 
-export const PORT = process.env.PORT;
+export const PORT = process.env.PORT || 3000;
 export const DB_HOST = process.env.DB_HOST || "srv826.hstgr.io";
-export const DB_USER = process.env.DB_USER;
+export const DB_USER = process.env.DB_USER || "u930882479_MF_Demo";
 export const DB_PORT = process.env.DB_PORT || "3306";
-export const DB_PASSWORD = process.env.DB_PASSWORD;
-export const DB_DATABASE = process.env.DB_DATABASE;
+export const DB_PASSWORD = process.env.DB_PASSWORD || "MF_DemoApi1.";
+export const DB_DATABASE = process.env.DB_DATABASE || "u930882479_DemoApi";
